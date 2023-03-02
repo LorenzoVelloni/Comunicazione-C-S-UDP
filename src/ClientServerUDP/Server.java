22859647b7c5d93f5afc38de5e23d5b4e8cea1a9
lorenzo Velloni <lorenzovelloni@gmail.com>
2023-03-02 08:55:55 +0100

    Client aggiornato

--- src/ClientServerUDP/Server.java
+++ src/ClientServerUDP/Server.java
@@ -1,66 +1,74 @@
 package ClientServerUDP;
+import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.net.MulticastSocket;
 /**
  *
  * @author LV
  */
-public class Server
-{
-  public static void main(String args[]) throws Exception 
-  {
-    // Creazione del socket del server sulla porta 6789
-    DatagramSocket serverSocket = new DatagramSocket(6789);
+public class Server {
+    public static void main(String args[]) throws IOException {
+        // Dichiarazione delle porte per la comunicazione unicast e multicast
+        int unicastPort = 6789;
 
-    // Variabile che tiene traccia dello stato del server
-    boolean attivo = true;
 
-    // Buffer per la ricezione e l'invio dei dati
-    byte[] bufferIN = new byte[1024];
-    byte[] bufferOUT = new byte[1024];
+        // Creazione delle socket per la comunicazione unicast e multicast
+        DatagramSocket unicastSocket = new DatagramSocket(unicastPort);
 
-    // Stampa un messaggio di avvio del server
-    System.out.println("SERVER avviato...");
+        // Buffer per la ricezione e l'invio dei dati
+        byte[] bufferIN = new byte[1024];
+        byte[] bufferOUT = new byte[1024];
 
-    // Ciclo principale del server, in attesa di ricevere dati dai client
-    while(attivo)
-    {
-      // Definizione del datagramma per ricevere dati dal client
-      DatagramPacket receivePacket = new DatagramPacket(bufferIN, bufferIN.length);
+        // Variabile che tiene traccia dello stato del server
+        boolean attivo = true;
 
-      // Attesa della ricezione del datagramma dal client
-      serverSocket.receive(receivePacket);
+        // Stampa un messaggio di avvio del server
+        System.out.println("SERVER avviato...");
 
-      // Analisi del pacchetto ricevuto
-      String ricevuto = new String(receivePacket.getData());
-      int numCaratteri = receivePacket.getLength();        
-      ricevuto = ricevuto.substring(0,numCaratteri); // Rimuove eventuali caratteri di padding
+        // Ciclo principale del server, in attesa di ricevere dati dai client
+        while (attivo) {
+            // Definizione del datagramma per ricevere dati dal client
+            DatagramPacket receivePacket = new DatagramPacket(bufferIN, bufferIN.length);
 
-      // Stampa del messaggio ricevuto dal client
-      System.out.println("RICEVUTO: " + ricevuto);
+            // Attesa della ricezione del datagramma dal client
+            if (Math.random() < 0.5) {
+                // Ricezione tramite unicast
+                unicastSocket.receive(receivePacket);
+            } 
 
-      // Riconoscimento degli attributi del socket del client (IP e porta)
-      InetAddress IPClient = receivePacket.getAddress();
-      int portaClient = receivePacket.getPort();
+            // Analisi del pacchetto ricevuto
+            String ricevuto = new String(receivePacket.getData());
+            int numCaratteri = receivePacket.getLength();
+            ricevuto = ricevuto.substring(0, numCaratteri); // Rimuove eventuali caratteri di padding
 
-      // Preparazione del messaggio da inviare al client
-      String daSpedire = ricevuto.toUpperCase();
-      bufferOUT = daSpedire.getBytes();
+            // Stampa del messaggio ricevuto dal client
+            System.out.println("RICEVUTO: " + ricevuto);
 
-      // Invio del datagramma al client
-      DatagramPacket sendPacket = new DatagramPacket(bufferOUT, bufferOUT.length, IPClient, portaClient);
-      serverSocket.send(sendPacket);
+            // Riconoscimento degli attributi del socket del client (IP e porta)
+            InetAddress IPClient = receivePacket.getAddress();
+            int portaClient = receivePacket.getPort();
 
-      // Controllo per la terminazione del server (in caso il messaggio ricevuto sia "fine")
-      if (ricevuto.equals("fine"))
-      {
-        System.out.println("SERVER IN CHIUSURA. Buona serata.");
-        attivo = false;
-      }  
+            // Preparazione del messaggio da inviare al client
+            String daSpedire = ricevuto.toUpperCase();
+            bufferOUT = daSpedire.getBytes();
+
+            // Invio del datagramma al client
+            DatagramPacket sendPacket = new DatagramPacket(bufferOUT, bufferOUT.length, IPClient, portaClient);
+            if (Math.random() < 0.5) {
+                // Invio tramite unicast
+                unicastSocket.send(sendPacket);
+            } 
+
+            // Controllo per la terminazione del server (in caso il messaggio ricevuto sia "fine")
+            if (ricevuto.equals("fine")) {
+                System.out.println("SERVER IN CHIUSURA. Buona serata.");
+                attivo = false;
+            }
+        }
+
+        // Chiusura delle socket del server
+        unicastSocket.close();
     }
-
-    // Chiusura del socket del server
-    serverSocket.close();
-  }
 }
