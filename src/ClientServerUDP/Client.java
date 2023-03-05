9554bb1e8475e10ba80c9e5121d41957d7004342
Lorenzo <lorenzovelloni@gmail.com>
2023-03-05 14:18:16 +0100

    Nuovo aggiornamento 5/03 + UDPMulticast

--- src/ClientServerUDP/Client.java
+++ src/ClientServerUDP/Client.java
@@ -3,61 +3,74 @@ import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.io.*;
+import java.net.MulticastSocket;
 /**
  *
  * @author   LV
  */
 public class Client {
-  public static void main(String args[]) throws Exception {
-  
-    // dichiarazione della porta del server
-    int portaServer = 6789;
-    
-    // dichiarazione dell'indirizzo IP del server
-    InetAddress IPServer = InetAddress.getByName("localhost");
+    public static void main(String args[]) throws Exception {
 
-    // dichiarazione dei buffer di spedizione e ricezione
-    byte[] bufferOUT = new byte[1024];
-    byte[] bufferIN = new byte[1024];
-    
-    // dichiarazione di un buffer di input
-    BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
-   
-    // creazione del socket del client
-    DatagramSocket clientSocket = new DatagramSocket();
+        // Dichiarazione della porta e dell'indirizzo Multicast
+        int portaMultiCast = 6789;
+        String IPServerMultiCast = "224.0.0.1";
 
-    // stampa del messaggio di avviso al client
-    System.out.println("Client pronto - inserisci un dato da inviare:");
+        // Dichiarazione della porta del server
+        int portaServer = 6790;
 
-    // lettura del dato in input dal client
-    String daSpedire = input.readLine();
-    
-    // conversione del dato in un array di byte
-    bufferOUT = daSpedire.getBytes();
-    
-    // creazione del pacchetto da spedire al server
-    DatagramPacket sendPacket = new DatagramPacket(bufferOUT, bufferOUT.length, IPServer, portaServer);
-    
-    // invio del pacchetto al server
-    clientSocket.send(sendPacket);
- 
-    // creazione del pacchetto per la ricezione dei dati dal server
-    DatagramPacket receivePacket = new DatagramPacket(bufferIN, bufferIN.length);
-    
-    // ricezione dei dati dal server
-    clientSocket.receive(receivePacket);
+        // Dichiarazione dell'indirizzo IP del server
+        InetAddress IPServer = InetAddress.getLocalHost();
 
-    // conversione dei dati ricevuti in una stringa
-    String ricevuto = new String(receivePacket.getData());
+        // Dichiarazione dei buffer di spedizione e ricezione
+        byte[] bufferOUT = new byte[1024];
+        byte[] bufferIN = new byte[1024];
 
-    // elaborazione dei dati ricevuti
-    int numCaratteri = receivePacket.getLength();        
-    ricevuto = ricevuto.substring(0, numCaratteri); //elimina i caratteri in eccesso 
+        // Dichiarazione di un buffer di input
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
 
-    // stampa del messaggio ricevuto dal server
-    System.out.println("dal SERVER:" + ricevuto);
- 
-    // chiusura del socket del client
-    clientSocket.close();
-  }
+        // Creazione della socket Multicast del client
+        MulticastSocket clientMultiCastSocket = new MulticastSocket(portaMultiCast);
+
+        // Aggiunta del client al gruppo di Multicast
+        InetAddress gruppo = InetAddress.getByName(IPServerMultiCast);
+        clientMultiCastSocket.joinGroup(gruppo);
+
+        // Creazione del socket del client per la comunicazione con il server
+        DatagramSocket clientSocket = new DatagramSocket();
+
+        // Stampa del messaggio di avviso al client
+        System.out.println("Client pronto - inserisci un dato da inviare:");
+
+        // Lettura del dato in input dal client
+        String daSpedire = input.readLine();
+
+        // Conversione del dato in un array di byte
+        bufferOUT = daSpedire.getBytes();
+
+        // Creazione del pacchetto da spedire al gruppo di Multicast
+        DatagramPacket sendMultiCastPacket = new DatagramPacket(bufferOUT, bufferOUT.length, gruppo, portaMultiCast);
+
+        // Invio del pacchetto al gruppo di Multicast
+        clientMultiCastSocket.send(sendMultiCastPacket);
+
+        // Creazione del pacchetto per la ricezione dei dati dal server
+        DatagramPacket receivePacket = new DatagramPacket(bufferIN, bufferIN.length);
+
+        // Ricezione dei dati dal server
+        clientSocket.receive(receivePacket);
+
+        // Conversione dei dati ricevuti in una stringa
+        String ricevuto = new String(receivePacket.getData());
+
+        // Elaborazione dei dati ricevuti
+        int numCaratteri = receivePacket.getLength();
+        ricevuto = ricevuto.substring(0, numCaratteri); // Elimina i caratteri in eccesso
+
+        // Stampa del messaggio ricevuto dal server
+        System.out.println("dal SERVER: " + ricevuto);
+
+        // Chiusura dei socket del client
+        clientSocket.close();
+        clientMultiCastSocket.close();
+    }
 }
--- src/ClientServerUDP/Server.java
+++ src/ClientServerUDP/Server.java
@@ -1,7 +1,5 @@
 package ClientServerUDP;
-import java.io.IOException;
 import java.net.DatagramPacket;
-import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.MulticastSocket;
 /**
@@ -9,66 +7,68 @@ import java.net.MulticastSocket;
  * @author LV
  */
 public class Server {
-    public static void main(String args[]) throws IOException {
-        // Dichiarazione delle porte per la comunicazione unicast e multicast
-        int unicastPort = 6789;
+    public static void main(String args[]) throws Exception {
+        // Indirizzo di multicast su cui inviare i messaggi
+        InetAddress group = InetAddress.getByName("224.0.0.1");
+        // Porta su cui inviare i messaggi
+        int port = 6789;
 
+        // Crea una MulticastSocket associata alla porta specificata
+        MulticastSocket multicastSocket = new MulticastSocket(port);
 
-        // Creazione delle socket per la comunicazione unicast e multicast
-        DatagramSocket unicastSocket = new DatagramSocket(unicastPort);
+        // Aggiunge il server al gruppo di multicast
+        multicastSocket.joinGroup(group);
 
-        // Buffer per la ricezione e l'invio dei dati
+        // Buffer di spedizione e ricezione dei pacchetti UDP.
         byte[] bufferIN = new byte[1024];
         byte[] bufferOUT = new byte[1024];
 
-        // Variabile che tiene traccia dello stato del server
-        boolean attivo = true;
-
-        // Stampa un messaggio di avvio del server
+        // Stampa un messaggio di avvio del server.
         System.out.println("SERVER avviato...");
 
-        // Ciclo principale del server, in attesa di ricevere dati dai client
-        while (attivo) {
-            // Definizione del datagramma per ricevere dati dal client
+        // Ciclo principale del server.
+        while (true) {
+            // Definisce un pacchetto UDP per la ricezione dei dati dal client.
             DatagramPacket receivePacket = new DatagramPacket(bufferIN, bufferIN.length);
 
-            // Attesa della ricezione del datagramma dal client
-            if (Math.random() < 0.5) {
-                // Ricezione tramite unicast
-                unicastSocket.receive(receivePacket);
-            } 
+            // Attende la ricezione di un pacchetto dal client.
+            multicastSocket.receive(receivePacket);
 
-            // Analisi del pacchetto ricevuto
+            // Converte i dati ricevuti in una stringa.
             String ricevuto = new String(receivePacket.getData());
-            int numCaratteri = receivePacket.getLength();
-            ricevuto = ricevuto.substring(0, numCaratteri); // Rimuove eventuali caratteri di padding
 
-            // Stampa del messaggio ricevuto dal client
+            // Determina la lunghezza del messaggio ricevuto.
+            int numCaratteri = receivePacket.getLength();
+
+            // Elimina eventuali caratteri in eccesso nella stringa ricevuta.
+            ricevuto = ricevuto.substring(0, numCaratteri);
+
+            // Stampa il messaggio ricevuto.
             System.out.println("RICEVUTO: " + ricevuto);
 
-            // Riconoscimento degli attributi del socket del client (IP e porta)
-            InetAddress IPClient = receivePacket.getAddress();
-            int portaClient = receivePacket.getPort();
-
-            // Preparazione del messaggio da inviare al client
+            // Prepara la stringa da inviare al client.
             String daSpedire = ricevuto.toUpperCase();
+
+            // Converte la stringa in un array di byte.
             bufferOUT = daSpedire.getBytes();
 
-            // Invio del datagramma al client
-            DatagramPacket sendPacket = new DatagramPacket(bufferOUT, bufferOUT.length, IPClient, portaClient);
-            if (Math.random() < 0.5) {
-                // Invio tramite unicast
-                unicastSocket.send(sendPacket);
-            } 
+            // Crea un pacchetto UDP per l'invio dei dati al client.
+            DatagramPacket sendPacket = new DatagramPacket(bufferOUT, bufferOUT.length, group, port);
 
-            // Controllo per la terminazione del server (in caso il messaggio ricevuto sia "fine")
+            // Invia il pacchetto al client.
+            multicastSocket.send(sendPacket);
+
+            // Controlla se il messaggio ricevuto è "fine" per terminare il server.
             if (ricevuto.equals("fine")) {
                 System.out.println("SERVER IN CHIUSURA. Buona serata.");
-                attivo = false;
+                break;
             }
         }
 
-        // Chiusura delle socket del server
-        unicastSocket.close();
+        // Rimuove il server dal gruppo di multicast
+        multicastSocket.leaveGroup(group);
+
+        // Chiude la socket del server.
+        multicastSocket.close();
     }
 }
